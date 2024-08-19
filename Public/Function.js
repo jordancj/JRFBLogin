eb93b3f75794b8bf64debe39aa1db8b132358ca7
jordancj <jordan-cj998@hotmail.com>
2024-08-19 19:12:08 +1000

    added session authentication and worked out bugs

--- Public/Function.js
+++ Public/Function.js
@@ -7,19 +7,23 @@ document.getElementById('login').addEventListener('submit', async (event) => {
         const response = await fetch('https://jrfblogin-a8dhhtczbwabe8at.australiaeast-01.azurewebsites.net/login', {
             method: 'POST',
             headers:{
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
+            credentials: 'include',
             body: JSON.stringify({
                 username:username
             })
         });
-        if (response.ok){
-            const result = await response.json();
-            console.log('Authentication successful:', result);
+        const data = await response.json();
+
+        if (response.ok && data.success){
+            sessionStorage.setItem('authToken', data.token);
+            console.log(data.token)
+            console.log('Authentication successful:', data);
             sessionStorage.setItem('username', username);
             window.location.href = "Selection.html";
         }else{
-            console.error('Authentication failed: ', response.statusText);
+            console.error('Authentication failed: ', data.statusText);
             alert("Invalid username, usernames must be between 3 and 20 characters and only contain a full stop no special characters. Please try again");
         }
     } catch(error) {
@@ -28,11 +32,12 @@ document.getElementById('login').addEventListener('submit', async (event) => {
     }
 });
 
-document.querySelectorAll('.activity').forEach(button =>{
+document.querySelectorAll('.activity button').forEach(button =>{
     button.addEventListener('click', function(event){
         const value = event.target.getAttribute('data-value');
+        console.log(value)
         sessionStorage.setItem('activitySelection', value);
-        if (value == 'operational'){
+        if (value == 'Operational'){
             window.location.href = "Operational.html"
         }
         else if(value == 'Non-Operational'){
